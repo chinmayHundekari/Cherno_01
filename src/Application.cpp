68c988c18bde3deea4788f78bbf106d40927d972
Chinmay <chinmay.hundekari@gmail.com>
2020-02-16 13:52:18 +0530

    Video 9

--- src/Application.cpp
+++ src/Application.cpp
@@ -104,17 +104,29 @@ int main(void)
     }
     std::cout << glGetString(GL_VERSION) << std::endl;
 
-    float positions[6] = {
+    float positions[] = {
         -0.5f, -0.5f,
-        0.0f, 0.5f,
-        0.5f, -0.5f
+         0.5f, -0.5f,
+         0.5f,  0.5f,
+        -0.5f,  0.5f,
+    };
+
+    int indices[] = {
+        0, 1, 2,
+        3, 0, 2
     };
     unsigned int buffer;
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 12*sizeof(float), positions, GL_STATIC_DRAW);
+
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+    unsigned int ibo; 
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
     ShaderProgramSource shaderProgram = parseShader("res/shaders/Basic.shader");
     unsigned int shader = CreateShader(shaderProgram.vertexSource, shaderProgram.fragmentSource);
@@ -126,7 +138,7 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
         
-        glDrawArrays(GL_TRIANGLES, 0, 3); // Draws the last glBindBuffer
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr); // Draws the last glBindBuffer
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
